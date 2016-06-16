4de64243ef139a844b4f9afe35c1eac04bb8229d
crodeheaver <colbyrodeheaver@hotmail.com>
2016-06-16 14:36:50 +0000

    removed https

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var port = process.env.PORT || 80
 var app = express();
-var https = require('https').Server(app);
-var io = require('socket.io')(https);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
@@ -14,6 +14,6 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-https.listen(port, function(){
+http.listen(port, function(){
   console.log('Example app listening on port ' + port);
 });

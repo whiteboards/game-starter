fea504c377ded3dbe3d70a1bd91cfb3a72b0bac8
Colby Rodeheaver <colbyrodeheaver@hotmail.com>
2016-06-20 20:27:54 -0500

    collision

--- src/client/sprites/Ship.js
+++ src/client/sprites/Ship.js
@@ -1,5 +1,5 @@
 import Phaser from 'phaser'
-import Bullets from './Bullets'
+// import Bullets from './Bullets'
 
 export default class extends Phaser.Sprite {
 
@@ -7,32 +7,31 @@ export default class extends Phaser.Sprite {
     super(game, x, y, asset)
 
     this.game = game
-    
-    this.weapon = game.add.weapon(30, 'bullet');
+
+    this.weapon = game.add.weapon(30, 'bullet')
 
     //  The bullets will be automatically killed when they are 2000ms old
-    this.weapon.bulletKillType = Phaser.Weapon.KILL_LIFESPAN;
-    this.weapon.bulletLifespan = 2000;
+    this.weapon.bulletKillType = Phaser.Weapon.KILL_LIFESPAN
+    this.weapon.bulletLifespan = 2000
 
     //  The speed at which the bullet is fired
-    this.weapon.bulletSpeed = 600;
+    this.weapon.bulletSpeed = 600
 
     //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 60ms
-    this.weapon.fireRate = 100;
+    this.weapon.fireRate = 100
 
     //  Wrap bullets around the world bounds to the opposite side
-    this.weapon.bulletWorldWrap = true;
-    
-    
+    this.weapon.bulletWorldWrap = true
+
     this.anchor.setTo(0.5)
 
     this.game.physics.arcade.enable(this)
+    this.game.physics.arcade.enable(this.weapon)
 
     this.body.drag.set(100)
     this.body.maxVelocity.set(200)
-    
-    this.weapon.trackSprite(this, 0, 0, true);
-    
+
+    this.weapon.trackSprite(this, 0, 0, true)
   }
 
   update () {
@@ -51,42 +50,33 @@ export default class extends Phaser.Sprite {
     }
 
     if (this.game.key_fire.isDown) {
-      this.weapon.fire();
+      this.weapon.fire()
     }
-    this.game.asteroids
-    
-    for(let i = 0; i < this.game.asteroids.length; i++)
-    {
-      this.game.physics.arcade.collide(this, this.game.asteroids[i], this.collisionCallback, this.processCallback, this);
-    }
-    for(let i = 0; i < this.game.asteroids.length; i++)
-    {
-      this.game.physics.arcade.collide(this.game.asteroids[i], this.weapon, this.collisionCallback, this.processCallback, this);
-    }
-    
-    
-    this.game.world.wrap(this, 16);
-  }
-  
-  processCallback (obj1, obj2) {
 
+    for (let i = 0; i < this.game.asteroids.length; i++) {
+      this.game.physics.arcade.collide(this, this.game.asteroids[i], this.collisionCallback, this.processCallback, this)
+    }
+    this.weapon.forEach(this.AsteroidColl, this)
+    this.game.world.wrap(this, 16)
+  }
+  AsteroidColl (sprite) {
+    for (let i = 0; i < this.game.asteroids.length; i++) {
+      this.game.physics.arcade.collide(sprite, this.game.asteroids[i], this.collisionCallback, this.processCallback, this)
+    }
+  }
+  processCallback (obj1, obj2) {
     //  This function can perform your own additional checks on the 2 objects that collided.
     //  For example you could test for velocity, health, etc.
     //  This function needs to return either true or false. If it returns true then collision carries on (separating the two objects).
     //  If it returns false the collision is assumed to have failed and aborts, no further checks or separation happen.
+    return true
+  }
 
+  collisionCallback (obj1, obj2) {
     
-        return true;
-    
-}
+  }
 
-collisionCallback (obj1, obj2) {
-
-    this.game.stage.backgroundColor = '#992d2d';
-
-}
-
-  render(){
+  render () {
     this.weapon.debug()
   }
 }
